6fd0e8b5413be0ba5605baa26f065727d8751113
quaan <quandev.fr@gmail.com>
2025-04-26 00:18:14 +0700

    setup deploy

--- local.js
+++ local.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+var bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  });
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors());
+app.use(morgan("common"));
 
 app.use("/api", userRoutes);
 
