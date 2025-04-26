d21fbde255ed1836a30b779e4ff0b05ba401f1c9
quaan <quandev.fr@gmail.com>
2025-04-26 23:54:05 +0700

    setup deploy

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
+const { json } = require("body-parser");
 const express = require("express");
 const app = express();
 
+const users = [
+  { id: 1, name: "Nguyễn Văn A", phone: "0123456789" },
+  { id: 2, name: "Trần Thị B", phone: "0987654321" },
+  { id: 3, name: "Lê Văn C", phone: "0909090909" },
+];
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.use("/api", userRoutes);
+app.get("/api/users", json(users));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
