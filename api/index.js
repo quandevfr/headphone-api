7aa2ceff8e008ca15cf363c06b06cfaccfaecefd
quaan <quandev.fr@gmail.com>
2025-04-26 23:50:39 +0700

    setup deploy

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  });
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.use("/api", userRoutes);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
